b352d3843ea33536c7aafd6f874605787a11d205
Jean Damke <jdamke@simscale.com>
2025-02-01 16:50:49 +0100

    Initial working version of rendering 3D object

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,131 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+import { Renderer, TextureLoader, THREE } from 'expo-three';
+import React from 'react';
+import { useEffect, useRef } from 'react';
+import { Text, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+/**
+ * Constants
+ */
+const CAMERA_Y_DISTANCE = 0
+const CAMERA_Z_DISTANCE = 20
 
 export default function HomeScreen() {
+  const timeoutRef = useRef<number>();
+  const sceneRef = useRef<THREE.Scene>();
+  const cameraRef = useRef<THREE.Camera>();
+
+
+  useEffect(() => {
+    // Clear the animation loop when the component unmounts
+    return () => clearTimeout(timeoutRef.current);
+  }, []);
+
+  const onContextCreate = (gl: ExpoWebGLRenderingContext) => {
+    // removes the warning EXGL: gl.pixelStorei() doesn't support this parameter yet!
+    const pixelStorei = gl.pixelStorei.bind(gl);
+    gl.pixelStorei = function (...args) {
+      const [parameter] = args;
+      switch (parameter) {
+        case gl.UNPACK_FLIP_Y_WEBGL:
+          return pixelStorei(...args);
+      }
+    };
+
+    /**
+     * Textures
+     */
+    const blueMistTexture = new TextureLoader().load(require('@/assets/textures/blueMist.jpg'));
+    const woodTexture = new TextureLoader().load(require('@/assets/textures/wood.jpg'));
+
+    /**
+     * Renderer
+     */
+    sceneRef.current = new THREE.Scene();
+    sceneRef.current.background = new THREE.Color(0xe0e0e0);
+    const renderer = new Renderer({ gl });
+
+    /**
+     * Camera
+     */
+    const cam = new THREE.PerspectiveCamera(
+      80,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      100
+    );
+    cam.position.set(0, CAMERA_Y_DISTANCE, CAMERA_Z_DISTANCE)
+    cam.up.set(0, 0, 1)
+    cameraRef.current = cam;
+
+    /**
+     * Dice
+     */
+    const diceGeometry = new THREE.IcosahedronGeometry(1)
+    const diceMaterial = new THREE.MeshStandardMaterial({
+      map: blueMistTexture,
+      transparent: true,
+      opacity: 0.6,
+      side: THREE.DoubleSide,
+      metalness: 0.2,
+      roughness: 0.5,
+    })
+    const dice = new THREE.Mesh(diceGeometry, diceMaterial)
+
+    dice.castShadow = true
+    dice.position.set(0, 0, 10)
+    dice.up.set(0, 0, 1)
+    sceneRef.current.add(dice)
+
+
+    // Table
+    const tableGeometry = new THREE.PlaneGeometry(50, 50)
+    const tableMaterial = new THREE.MeshStandardMaterial({
+      map: woodTexture,
+      metalness: 0.2,
+      roughness: 0.5,
+    })
+    const table = new THREE.Mesh(tableGeometry, tableMaterial)
+    table.receiveShadow = true
+    sceneRef.current.add(table)
+
+    /**
+     * Lights
+     */
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2.5)
+    sceneRef.current.add(ambientLight)
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4)
+    directionalLight.castShadow = true
+    directionalLight.shadow.mapSize.set(1024, 1024)
+    directionalLight.shadow.camera.far = 25
+    directionalLight.shadow.camera.left = -25
+    directionalLight.shadow.camera.top = 25
+    directionalLight.shadow.camera.right = 25
+    directionalLight.shadow.camera.bottom = -25
+    directionalLight.position.set(0, 0, 20)
+    sceneRef.current.add(directionalLight)
+
+    const animate = () => {
+      timeoutRef.current = requestAnimationFrame(animate);
+
+      renderer.render(sceneRef.current, cameraRef.current);
+
+      gl.endFrameEXP();
+    }
+
+    animate();
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={{ flex: 1 }}>
+      <Text style={{ marginTop: 100, marginLeft: 20, fontWeight: 'bold' }}>3D Dice roll</Text>
+      <GLView
+        style={{ flex: 1 }}
+        onContextCreate={onContextCreate}
+      />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+
